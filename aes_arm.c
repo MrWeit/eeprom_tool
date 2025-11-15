2371f67d71035947857b919616121f78ad380d9d
AnatolyGeorgievski <anatoly.georgievski@gmail.com>
2025-11-15 16:21:15 +0300

    AES CBC mode для Aarch64+crypto

---
+++ aes_arm.c
@@ -0,0 +1,204 @@
+/*! 
+ 	\author _Aнатолий Георгиевский_ (https://github.com/AnatolyGeorgievski)
+
+	 * # of rounds specified by AES:
+	 * 128 bit key		10 rounds
+	 * 192 bit key		12 rounds
+	 * 256 bit key		14 rounds
+	 * => n byte key	=> 6 + (n/4) rounds
+
+	\see https://nvlpubs.nist.gov/nistpubs/fips/nist.fips.197.pdf
+
+$ gcc -DTEST_AES -march=native -o aes_arm  aes_arm.c
+
+$ gcc -DTEST_AES -march=armv8-a+crypto -o aes_arm  aes_arm.c 
+
+$ clang --target=aarch64 -march=armv8-a+crypto -O3 -S -o aes_arm.s  aes_arm.c
+
+Тест пропускной способности на симуляторе загрузки ядра
+$ llvm-mca --march=aarch64 --mcpu=cortex-a57 -timeline aes_arm.s
+ */
+#include <stdint.h>
+
+typedef struct _Cipher Cipher_t;
+struct _Cipher {
+	const uint8_t *key;
+	const uint8_t *subkeys;
+	uint8_t iv[16];
+};
+
+# if defined(__ARM_NEON)
+#  include <arm_neon.h>
+# endif
+/* GCC and LLVM Clang, but not Apple Clang */
+# if defined(__ARM_ACLE) || defined(__ARM_FEATURE_CRYPTO)
+#   include <arm_acle.h>
+# endif
+
+/*
+ * aes_sub() - use the aese instruction to perform the AES sbox substitution
+ *             on each byte in 'input'
+ */
+static inline uint32_t SubWord(uint32_t x)
+{
+	uint8x16_t v = {0};
+	uint8x16_t k = vreinterpretq_u8_u32(vdupq_n_u32(x));
+	v = vaeseq_u8 (v,k);
+	return vgetq_lane_u32(vreinterpretq_u32_u8(v), 0);
+}
+static inline uint8x16_t InvMixColumns4(uint8x16_t v){
+	return vaesimcq_u8(v);
+}
+typedef struct _AES_Ctx AES_Ctx;
+struct _AES_Ctx {
+	uint8x16_t  K[10+1];
+	uint8x16_t iv;
+};
+
+static inline uint8x16_t aes_encrypt_block(AES_Ctx * ctx, uint8x16_t v, const int nr)
+{
+	v = vaeseq_u8(v, ctx->K[0]);
+	for (unsigned int i=1; i<nr; ++i) {
+		v = vaesmcq_u8(v);
+		v = vaeseq_u8(v, ctx->K[i]);
+	}
+	return veorq_u8(v, ctx->K[nr]);
+}
+
+void AES_EBC_128_encrypt(AES_Ctx*ctx, uint8_t* dst, const uint8_t* src, int length)
+{
+	const int rounds = 10;// важно чтобы это была константа 
+    uint8x16_t d;
+    int blocks = length>>4;
+    for (int i=0;i<blocks;i++) {
+        d = vld1q_u8(src+i*16);
+        d = aes_encrypt_block(ctx, d, rounds);
+		vst1q_u8(dst+i*16, d);
+    }
+}
+void AES_CBC_128_encrypt(AES_Ctx*ctx, uint8_t* dst, const uint8_t* src, int length)
+{
+	const int rounds = 10;// важно чтобы это была константа 
+    uint8x16_t d, v;
+    v = ctx->iv;
+    int blocks = length>>4;
+    for (int i=0;i<blocks;i++) {
+        d = veorq_u8(v, vld1q_u8(src+i*16));
+        v = aes_encrypt_block(ctx, d, rounds);
+		vst1q_u8(dst+i*16, v);
+    }
+}
+#define ROTL(x,n) ((x)<<(n)) ^ ((x)>>(32-(n)))
+#define ROTR(x,n) ((x)>>(n)) ^ ((x)<<(32-(n)))
+
+/*! AES-128 разгибание ключа
+    Nk -- длина ключа 4 слова (128 бит)
+ */
+static void KeyExpansion(AES_Ctx * ctx, const uint8_t* key, int ekb)
+{
+    uint32_t *w = (uint32_t*)ctx->K;//,
+    int Nk = (ekb&0xFFFF)/32;
+    int Nbr = 4*(7+Nk);
+    uint32_t rcon = 1;
+    int i;
+	ctx->K[0] = vld1q_u8(key);
+    for (i = Nk;i < Nbr/*Nb*(Nr+1)*/; i++)
+    {
+        uint32_t temp = w[i-1];
+        if ((i%Nk)==0)//Nk)
+        {
+            temp = (ROTR(SubWord(temp),8)) ^ rcon;
+            rcon <<=1;
+            if (rcon & 0x100) rcon ^= 0x11b;
+        } else if (Nk==8 && (i&7) == 4)
+        {
+            temp = SubWord(temp);
+        }
+        w[i] = w[i-Nk] ^ temp;
+    }
+    if (ekb>>16) {
+        //InvMixColumns(&w[4],4*(Nk+5));//-Nk-4);
+        for (i=1;i<(6+Nk);i++){
+            ctx->K[i] = InvMixColumns4(ctx->K[i]);
+		}
+    }
+}
+#if defined(TEST_AES)
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	/* FIPS 197, Appendix B input */
+	const uint8_t input[16] = { /* user input, unaligned buffer */
+		0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34
+	};
+
+	/* FIPS 197, Appendix B key */
+	const uint8_t key[16] = { /* user input, unaligned buffer */
+		0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x9 , 0xcf, 0x4f, 0x3c
+	};
+
+	/* FIPS 197, Appendix B expanded subkeys */
+	__attribute__((aligned(4)))
+	const uint8_t subkeys[10][16] = { /* library controlled, aligned buffer */
+		{0xA0, 0xFA, 0xFE, 0x17, 0x88, 0x54, 0x2c, 0xb1, 0x23, 0xa3, 0x39, 0x39, 0x2a, 0x6c, 0x76, 0x05},
+		{0xF2, 0xC2, 0x95, 0xF2, 0x7a, 0x96, 0xb9, 0x43, 0x59, 0x35, 0x80, 0x7a, 0x73, 0x59, 0xf6, 0x7f},
+		{0x3D, 0x80, 0x47, 0x7D, 0x47, 0x16, 0xFE, 0x3E, 0x1E, 0x23, 0x7E, 0x44, 0x6D, 0x7A, 0x88, 0x3B},
+		{0xEF, 0x44, 0xA5, 0x41, 0xA8, 0x52, 0x5B, 0x7F, 0xB6, 0x71, 0x25, 0x3B, 0xDB, 0x0B, 0xAD, 0x00},
+		{0xD4, 0xD1, 0xC6, 0xF8, 0x7C, 0x83, 0x9D, 0x87, 0xCA, 0xF2, 0xB8, 0xBC, 0x11, 0xF9, 0x15, 0xBC},
+		{0x6D, 0x88, 0xA3, 0x7A, 0x11, 0x0B, 0x3E, 0xFD, 0xDB, 0xF9, 0x86, 0x41, 0xCA, 0x00, 0x93, 0xFD},
+		{0x4E, 0x54, 0xF7, 0x0E, 0x5F, 0x5F, 0xC9, 0xF3, 0x84, 0xA6, 0x4F, 0xB2, 0x4E, 0xA6, 0xDC, 0x4F},
+		{0xEA, 0xD2, 0x73, 0x21, 0xB5, 0x8D, 0xBA, 0xD2, 0x31, 0x2B, 0xF5, 0x60, 0x7F, 0x8D, 0x29, 0x2F},
+		{0xAC, 0x77, 0x66, 0xF3, 0x19, 0xFA, 0xDC, 0x21, 0x28, 0xD1, 0x29, 0x41, 0x57, 0x5c, 0x00, 0x6E},
+		{0xD0, 0x14, 0xF9, 0xA8, 0xC9, 0xEE, 0x25, 0x89, 0xE1, 0x3F, 0x0c, 0xC8, 0xB6, 0x63, 0x0C, 0xA6}
+	};
+
+	/* Result */
+	uint8_t result[19] = { 0 };
+
+//	struct _Cipher ctx = {.key= key, .subkeys = (const uint8_t*)subkeys };
+	struct _AES_Ctx aes_ctx;
+	KeyExpansion(&aes_ctx, key, 128);
+	AES_EBC_128_encrypt(&aes_ctx, result+3, input, 16);
+
+	printf("Input: ");
+	for (unsigned int i=0; i<16; ++i)
+		printf("%02X ", input[i]);
+	printf("\n");
+
+	printf("Key: ");
+	for (unsigned int i=0; i<16; ++i)
+		printf("%02X ", key[i]);
+	printf("\n");
+
+	printf("Output: ");
+	for (unsigned int i=3; i<19; ++i)
+		printf("%02X ", result[i]);
+	printf("\n");
+
+	/* FIPS 197, Appendix B output */
+	const uint8_t exp[16] = {
+		0x39, 0x25, 0x84, 0x1D, 0x02, 0xDC, 0x09, 0xFB, 0xDC, 0x11, 0x85, 0x97, 0x19, 0x6A, 0x0B, 0x32
+	};
+
+	if (0 == memcmp(result+3, exp, 16))
+		printf("SUCCESS!!!\n");
+	else
+		printf("FAILURE!!!\n");
+
+/* проверка генерации subkeys
+	KeyExpansion(&aes_ctx, key, 128);
+
+	for (int r = 0; r<=10; r++){
+		uint8x16_t v = aes_ctx.K[r];
+		for (int i = 0; i<16; i++)
+			printf("0x%02X, ", v[i]);
+		printf("\n");
+	}
+		*/
+	return 0;
+}
+
+#endif
